1cf08d4103f441c0c12bdaddd3101067f9fc07c3
Eugene Zemtsov <Eugene.Zemtsov@microsoft.com>
2015-03-19 21:04:28 -0700

    load lib

--- dbg.cpp
+++ dbg.cpp
@@ -9,34 +9,41 @@
 #include "cordebug.h"
 
 
-
+#define DLL_PROCESS_ATTACH 1
 typedef HRESULT (*CreateCordbObject)(int iDebuggerVersion, ICorDebug **ppCordb);
+typedef BOOL (*DllMain)(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved);
+
+void *load_lib(const char *name)
+{
+    void *lib = dlopen(name, RTLD_LAZY | RTLD_GLOBAL);
+    if (lib == nullptr) 
+    {
+        printf("Can't load library %s\n", name);
+        return nullptr;
+    }
+
+    DllMain dllMainProc = (DllMain)dlsym(lib, "DllMain");
+    if (dllMainProc == nullptr) 
+    {
+        printf("Can't find DllMain in library %s\n", name);
+        return nullptr;
+    }
+    
+    if (!dllMainProc(lib, DLL_PROCESS_ATTACH, nullptr))
+    {
+        printf("DllMain for %s returned FALSE\n", name);
+        return nullptr;
+    }
+
+    return lib;
+}
 
 int main(int argc, const char **args)
 {
     printf("Hi, I'm a process %d\n", getpid());
 
-    void *dac_lib = dlopen("libmscordaccore.so", RTLD_LAZY | RTLD_GLOBAL);
-    if (dac_lib == nullptr) 
-    {
-        printf("Bummer DAC\n");
-        return 1;
-    }
-    else
-    {
-        printf("dac %p\n", dac_lib);
-    }
-
-    void *dbi_lib = dlopen("libmscordbi.so", RTLD_LAZY | RTLD_GLOBAL);
-    if (dbi_lib == nullptr) 
-    {
-        printf("Bummer\n");
-        return 1;
-    }
-    else
-    {
-        printf("dbi %p\n", dbi_lib);
-    }
+    void *dac_lib = load_lib("libmscordaccore.so");
+    void *dbi_lib = load_lib("libmscordbi.so");
 
     CreateCordbObject createCordbObject = (CreateCordbObject)dlsym(dbi_lib, "CreateCordbObject");
     if (createCordbObject == nullptr) 
