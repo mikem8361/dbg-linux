af1a192b70f10c294cb2f9a8d53bf476eba1487c
Mike McLaughlin <mikem@microsoft.com>
2015-09-06 13:12:06 -0700

    More build fixes.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 export CORECLR_SRC=../coreclr/src
-mkdir bin
+mkdir bin > /dev/null 2>&1
 clang++-3.5 -DPAL_IMPLEMENTATION -DFEATURE_PAL -D__LINUX__ -DPLATFORM_UNIX -DPAL_STDCPP_COMPAT -I$CORECLR_SRC/pal/inc -pthread -std=c++11 -g dbg.cpp cordebug_i.cpp -o bin/dbg -ldl -L$CORECLR_SRC/../bin/Product/Linux.x64.Debug/lib  -lunwind -lpalrt -lcoreclrpal 
 
--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
-export LD_LIBRARY_PATH=/home/eugene/projects/coreclr/bin/Product/Linux.x64.Debug
+export LD_LIBRARY_PATH=$HOME/projects/coreclr/bin/Product/Linux.x64.Debug
 export COMPlus_DbgWaitForDebuggerAttach=1
 #export COMPlus_LogEnable="1"
 #export COMPlus_LogFacility="200"
 #export COMPlus_LogLevel="10"
 #export COMPlus_LogToFile="1"
 
-./dbg
+./bin/dbg $@
